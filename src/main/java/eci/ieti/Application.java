44e0f7111e54d6aa08e44c6b16f0211d0a80897e
acai-bjca <amalia.alfonso@mail.escuelaing.edu.co>
2020-03-18 00:59:16 -0500

    se realizo la configuracion apr onectarse con mongodb per on ofunciona

--- src/main/java/eci/ieti/Application.java
+++ src/main/java/eci/ieti/Application.java
@@ -8,12 +8,20 @@ import eci.ieti.data.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
+//@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class Application implements CommandLineRunner {
-
+    
+    @Autowired
+    MongoTemplate mongoTemplate;
 
     @Autowired
     private CustomerRepository customerRepository;
